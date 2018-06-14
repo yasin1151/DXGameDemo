bf84354bb2394150675b7ef03edb6e7dbcccc730
yasin1151 <yasin1151@outlook.com>
2018-06-14 20:45:42 +0800

    完成窗口创建

--- DXGameDemo/main.cpp
+++ DXGameDemo/main.cpp
@@ -1,9 +1,94 @@
 #include <Windows.h>
+#include <assert.h>
+
+// 全局变量定义
+#define WINDOW_CLASS_NAME "DXGameDemo"
+#define WINDOW_TITLE "DXGame"
+#define WINDOW_HEIGHT 600
+#define WINDOW_WIDTH 800
+
+// 窗口响应函数
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, LPSTR lpCmdLine, int nShowCMd)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, LPSTR lpCmdLine, int nShowCmd)
 {
 
+	UNREFERENCED_PARAMETER(hPreInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	// 创建窗口类结构体
+	WNDCLASSEX wndClass = {};
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.style = CS_HREDRAW | CS_VREDRAW;
+	wndClass.lpfnWndProc = WndProc;
+	wndClass.cbClsExtra = 0;
+	wndClass.cbWndExtra = 0;
+	wndClass.hInstance = hInstance;
+	wndClass.hIcon = NULL;
+	wndClass.hCursor = NULL;
+	wndClass.hbrBackground = (HBRUSH)GetStockObject(GRAY_BRUSH);
+	wndClass.lpszMenuName = NULL;
+	wndClass.lpszClassName = WINDOW_CLASS_NAME;
+
+	// 注册窗口类
+	if (!RegisterClassEx(&wndClass))
+	{
+		return -1;
+	}
+
+	// 创建窗口
+	HWND hWnd = CreateWindow(
+		WINDOW_CLASS_NAME,
+		WINDOW_TITLE,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		WINDOW_WIDTH,
+		WINDOW_HEIGHT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	// 移动窗口
+	MoveWindow(hWnd, 250, 80, WINDOW_WIDTH, WINDOW_HEIGHT, true);
+	ShowWindow(hWnd, nShowCmd);
+	UpdateWindow(hWnd);
+
+	// 开始消息循环
+	MSG msg = {};
+	while (WM_QUIT != msg.message)
+	{
+		if (PeekMessage(&msg, NULL, NULL, NULL, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			
+		}
+	}
+
+	// 注销窗口
+	UnregisterClass(WINDOW_CLASS_NAME, wndClass.hInstance);
+	return 0;
+}
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch(uMsg)
+	{
+	case WM_PAINT:
+		ValidateRect(hWnd, NULL);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	}
 	return 0;
 }
